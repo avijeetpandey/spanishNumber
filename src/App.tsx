e8b4d8fb4daa8f8f7535b207632f99bea55c633b
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-09-04 16:56:30 +0530

    Add: sroll view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import {View, ScrollView, SafeAreaView, Text} from 'react-native';
-
-import Sound from 'react-native-sound';
+import {
+  View,
+  ScrollView,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 
 // list of sounds to play
 const soundList = [
@@ -14,18 +19,58 @@ const soundList = [
   require('./../assets/seven.wav'),
   require('./../assets/eight.wav'),
   require('./../assets/nine.wav'),
+  require('./../assets/ten.wav'),
 ];
 
 function App() {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <Text>Spaninsh App</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView style={styles.container}>
+      <Image source={require('../assets/logo.png')} style={styles.logo} />
+      <View style={styles.gridContainer}>
+        {soundList.map(sound => {
+          return (
+            <TouchableOpacity key={sound} onPress={() => {}} style={styles.box}>
+              <Text style={styles.text}>{sound}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1b262c',
+  },
+  logo: {
+    alignSelf: 'center',
+    marginTop: 15,
+  },
+  gridContainer: {
+    flex: 1,
+    margin: 5,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignItems: 'flex-start',
+  },
+  box: {
+    height: 110,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0f4c75',
+    width: '46%',
+    marginVertical: 6,
+    borderRadius: 6,
+    shadowColor: '#393e46',
+    elevation: 6,
+  },
+  text: {
+    fontSize: 50,
+    color: '#ff4301',
+  },
+});
 
 export default App;
