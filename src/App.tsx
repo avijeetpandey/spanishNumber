98779ebdde300005c59afd130941a099498bee29
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-09-04 16:26:29 +0530

    Add: sound assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import {View, ScrollView, SafeAreaView, Text} from 'react-native';
 
+import Sound from 'react-native-sound';
+
+// list of sounds to play
+const soundList = [
+  require('./../assets/one.wav'),
+  require('./../assets/two.wav'),
+  require('./../assets/three.wav'),
+  require('./../assets/four.wav'),
+  require('./../assets/five.wav'),
+  require('./../assets/six.wav'),
+  require('./../assets/seven.wav'),
+  require('./../assets/eight.wav'),
+  require('./../assets/nine.wav'),
+];
+
 function App() {
   return (
     <SafeAreaView>
