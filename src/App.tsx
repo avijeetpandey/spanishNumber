0e62e41c6df1fa02b065f7e1aa6ec06e5693241c
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-09-04 16:19:35 +0530

    Add: Sound player pod

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, ScrollView, SafeAreaView, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Spaninsh App</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
